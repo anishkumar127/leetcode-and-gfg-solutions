ea3eb59077b3c34cdbcab8067e211fac5e2dce06
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-01-05 14:19:03 +0530

    Time: 11 ms (18.72%), Space: 48.5 MB (5.68%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -1,13 +1,40 @@
 class Solution {
-    public int[] answerQueries(int[] A, int[] queries) {
-          Arrays.sort(A);
-        int n = A.length, m = queries.length, res[] = new int[m];
-        for (int i = 1; i < n; ++i)
-            A[i] += A[i - 1];
-        for (int i = 0; i < m; ++i) {
-            int j = Arrays.binarySearch(A, queries[i]);
-            res[i] = Math.abs(j + 1);
+    public int[] answerQueries(int[] nums, int[] queries) {
+        // sort 
+        Arrays.sort(nums);
+        
+        // prefix sum.
+        for(int i=1; i<nums.length; i++){
+            nums[i]+=nums[i-1]; 
         }
-        return res;
+        
+        int ans [] = new int [queries.length];
+        
+        for(int i=0; i<queries.length; i++){
+            
+            int q = queries[i];
+            
+            // apply binary search.
+            int low = 0,  high = nums.length-1;
+            
+            while(low <= high){
+                int mid = low + (high-low)/2;
+                
+                if(nums[mid]==q){
+                    ans[i] = mid + 1;
+                    break;
+                    
+                }else if(nums[mid] < q){
+                    
+                    ans[i] = mid + 1;
+                    low = mid + 1;
+                    
+                }else{
+                    high = mid - 1;
+                }
+            }
+            
+        }
+        return ans;
     }
 }
