7a1d34c0e7aff952d6a3348c84f3e67d35520ba2
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-02-12 21:15:43 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (25.25%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        
+        int [] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return findMax(0,nums,dp);
+    }
+    
+    private int findMax(int idx, int[] arr,int[] dp){
+        if(idx==arr.length) return 0;
+        
+        int rob = arr[idx];
+        int notRob = 0;
+        if(dp[idx]!=-1) return dp[idx];
+        
+        if(idx+2<=arr.length)
+            rob+=findMax(idx+2,arr,dp);
+        
+        notRob+=findMax(idx+1,arr,dp);
+        
+        int ans = Math.max(rob,notRob);
+        dp[idx] = ans;
+        return dp[idx];
+    }
+}
