94193963dac2d1d598ab08e6ffa001587a616955
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-02-12 16:54:37 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (51.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -10,19 +10,6 @@ class Solution {
             s = t;
         }
         return t;
-        // int[] dp = new int[n+1];
-        // return stairs(n,dp);
     }
-    private int stairs(int n, int[] dp){
-         
-        if(n==0) return 1;
-      
-        if(n<0) return 0;
-        
-        if(dp[n]!=0) return dp[n];
-        
-        int ans = stairs(n-1,dp) + stairs(n-2,dp);
-        dp[n] = ans;
-        return dp[n];
-    }
+
 }
