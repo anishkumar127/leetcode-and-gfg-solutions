b889010ad52a1f75626fcdce8a03dacb18f81e46
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-02-13 22:00:08 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (33.09%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,32 +1,19 @@
 class Solution {
-    private int minCost(int[] cost ,  int[] dp){
-      for(int idx = cost.length; idx>=0; idx--){
-          
-        if(idx==cost.length){
-             dp[idx] = 0;
-            continue;
+    private int minCost(int[] cost){
+        int n = cost.length;
+        
+        int idxp1 = cost[n-1];
+        int idxp2 = 0;
+        
+        for(int idx = n-2; idx>=0; idx--){
+            int ansIdx = cost[idx] + Math.min(idxp1,idxp2);
+            
+            idxp2 = idxp1;
+            idxp1 = ansIdx;
         }
-        
-        // step one
-        int stepOne = dp[idx+1];//minCost(cost,idx+1,dp);
-        
-        // step two.
-        // why this ? because maybe be the cost array not have enough elements. and we need to math.min
-        int stepTwo = Integer.MAX_VALUE;
-        
-        if(idx+2<=cost.length){
-            stepTwo =   dp[idx+2];//minCost(cost,idx+2,dp);
-        }
-        // cost[idx] because i need to current index cost also.
-        int ans = cost[idx] + Math.min(stepOne, stepTwo);
-        
-        dp[idx] = ans;
-      }  
-        
-        return Math.min(dp[0],dp[1]);
+        return Math.min(idxp1,idxp2);
     }
     public int minCostClimbingStairs(int[] cost) {
-        int n = cost.length;
-        return minCost(cost,new int [n+1]);
+        return minCost(cost);
     }
 }
