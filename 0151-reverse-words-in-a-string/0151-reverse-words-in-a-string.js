f2f673bdf9e8643b5913079750440979171ead6a
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-02-14 15:17:41 +0530

    Time: 71 ms (54.12%), Space: 44.3 MB (29.05%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -1,13 +1,8 @@
-class Solution {
-    public String reverseWords(String s) {
-        String[] words = s.split(" +"); // + because every type of space remove.
-        
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i=words.length-1; i>=0; i--){
-            sb.append(words[i]);
-            sb.append(" ");
-        }
-        return sb.toString().trim();
-    }
-}
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    return s.trim().split(/\s+/).reverse().join(' ');
+    
+};
