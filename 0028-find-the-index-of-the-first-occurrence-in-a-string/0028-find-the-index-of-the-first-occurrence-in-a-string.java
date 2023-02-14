daca53bd646ee3f785608d39aa2ba08d78648b59
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-02-14 14:48:27 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (47.37%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -12,3 +12,17 @@ class Solution {
         return -1;
     }
 }
+
+/*
+
+haystack.length() - needle.length() + 1; // why ? because sad start from s. and if s in the end of the length of haystack. then how we can get the substring ? its give the error out of bound.
+
+so we run till the haystack length and - needle length + 1.
+
+
+and now inside for loop we will check if haystack character match the first character of the needle.
+then we check the complete sentence of the needle using substring method. and we apply the substring method inside the haystack.
+and if it is equal then return the index.
+else not equal we check till the length. in the end we return the -1 as given the question requirements.
+
+*/
