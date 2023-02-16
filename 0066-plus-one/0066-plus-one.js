db17907a7d7f9620c6105377f873fbc01562bc95
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-02-16 15:34:58 +0530

    Time: 67 ms (56.31%), Space: 42.4 MB (11.43%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(arr) {
+    for(let i=arr.length-1; i>=0; i--){
+        if(arr[i]<9){
+            arr[i]++;
+            return arr;
+        }
+        arr[i] = 0;
+    }
+    arr.unshift(1);
+    return arr;
+};
