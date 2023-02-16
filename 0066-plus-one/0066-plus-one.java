a63425c41e92068e8f094ed2d1dfe87cb40e768c
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-02-16 15:32:26 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (22.18%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -13,3 +13,12 @@ class Solution {
         return arr;
     }
 }
+
+/*
+if number less then 9 then we simply add 1. and return the array.
+
+then one and only case occure when number is 9. and then we just need to make that 9 into 0.
+
+and then just increase the array length. and then add just extra 1 front of array. 
+and then return the array.
+*/
