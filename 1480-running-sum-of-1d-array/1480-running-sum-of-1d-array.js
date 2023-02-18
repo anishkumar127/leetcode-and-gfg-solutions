3af19220c6277762b1a18d7cd392829e11c3a01e
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-02-18 21:31:32 +0530

    Time: 54 ms (97.54%), Space: 41.1 MB (99.99%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    let sum =0;
+    let arr = [];
+    for(let i =0; i<nums.length; i++){
+        sum+=nums[i];
+        arr[i] = sum;
+    }
+    return arr;
+};
