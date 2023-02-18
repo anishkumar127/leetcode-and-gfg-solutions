d8dd65cad659890773c1459f4c61807afede9c35
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-02-18 14:48:18 +0530

    Time: 57 ms (93.02%), Space: 42.4 MB (47.90%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -13,12 +13,11 @@
 var invertTree = function(root) {
   if(root===null) return null;
     
-  invertTree(root.left);
-  invertTree(root.right);
+ let   lhs  = invertTree(root.left);
+  let  rhs = invertTree(root.right);
     
- let temp = root.left;
-    root.left = root.right;
-    root.right = temp;
+    root.left = rhs;
+    root.right = lhs;
         
     return root;
 };
