7854c5c8e6f10c44bb976413483984d12db08646
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-02-21 17:46:09 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (54.84%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,31 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int [] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return findMax(0,nums,dp);
+		  int[] dp = new int[n+1];
+		Arrays.fill(dp,-1);
+        return RobberMemo(nums,0,dp);
     }
-    
-    private int findMax(int idx, int[] arr,int[] dp){
-        for(idx = arr.length; idx>=0; idx--){
+     private static int RobberMemo(int[] arr, int idx, int[] dp){
         if(idx==arr.length){
-            dp[idx] = 0;
-            continue;
+            return dp[idx] = 0;
         }
-        
-        int rob = arr[idx];
-        int notRob = 0;
+        int take = arr[idx];
+        int notTake = 0;
         if(dp[idx]!=-1) return dp[idx];
-        
-        if(idx+2<=arr.length)
-            rob+=dp[idx+2]; //findMax(idx+2,arr,dp);
-        
-        notRob+=dp[idx+1]; //findMax(idx+1,arr,dp);
-        
-        int ans = Math.max(rob,notRob);
-        dp[idx] = ans;
+        if(idx+2<=arr.length){
+            take+=RobberMemo(arr,idx+2,dp);
         }
-        
-        return dp[0];
+        notTake+= RobberMemo(arr,idx+1,dp);
+        int ans = Math.max(take,notTake);
+        return dp[idx] = ans;
     }
 }
