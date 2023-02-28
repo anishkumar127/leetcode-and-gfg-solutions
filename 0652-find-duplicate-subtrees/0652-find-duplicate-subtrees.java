8d16a3a951300e818b2a77cea8a3d5ef9cc43bb4
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-02-28 16:06:22 +0530

    Time: 12 ms (91.52%), Space: 43.3 MB (90.17%) - LeetHub

---
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
@@ -0,0 +1,25 @@
+class Solution {
+   int curId = 1;
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        Map<String, Integer> serialToId = new HashMap<>();
+        Map<Integer, Integer> idToCount = new HashMap<>();
+        List<TreeNode> res = new LinkedList<>();
+        postorder(root, serialToId, idToCount, res);
+        return res;
+    }
+    
+    private int postorder(TreeNode root, Map<String, Integer> serialToId, Map<Integer, Integer> idToCount, List<TreeNode> res) {
+        if (root == null) return 0;
+        int leftId = postorder(root.left, serialToId, idToCount, res);
+        int rightId = postorder(root.right, serialToId, idToCount, res);
+        String curSerial = leftId + "," + root.val + "," + rightId;
+        int serialId = serialToId.getOrDefault(curSerial, curId);
+        if (serialId == curId) curId++;
+        serialToId.put(curSerial, serialId);
+        idToCount.put(serialId, idToCount.getOrDefault(serialId, 0) + 1);
+        if (idToCount.get(serialId) == 2) res.add(root);
+        return serialId;
+    }
+    
+}
