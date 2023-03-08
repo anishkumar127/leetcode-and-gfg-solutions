cd38f696fc3c737fe473ac533b5e903ffd7c8447
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-03-08 22:42:28 +0530

    Time: 108 ms (43.98%), Space: 54.3 MB (44.25%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,9 +3,14 @@ var containsDuplicate = function(nums) {
     
     for(var i = 0; i < nums.length; i++){
         obj[nums[i]] = obj[nums[i]] + 1 || 1;
-        
         if(obj[nums[i]] > 1) return true;
     }
-    
     return false;
 };
+
+// obj keeps track of the occurrences of each num in nums. As we loop through nums, we record its frequency.
+
+// If we've already recorded the frequency of num[i] before, then the frequency will simply be incremented by one.
+// If we have not recorded the frequency of nums[i] before, then undefined + 1 will be NaN. Then it becomes NaN || 1 which should evaluate to 1.
+
+// If at any point the frequency recorded for nums[i] is greater than 1, we return true.
