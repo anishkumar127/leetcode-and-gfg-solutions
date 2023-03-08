e33254c6d19615efbd74e501dd47cfe6e11378f0
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-03-08 22:32:19 +0530

    Time: 82 ms (94.72%), Space: 55.5 MB (18.84%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,13 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const set = new Set();
-    
-    for(let val of nums){
-        if(set.has(val)){
-            return true;
-        }else
-            set.add(val);
-    }
-    return false;
+    const set = new Set(nums);
+    return set.size!=nums.length;
 };
