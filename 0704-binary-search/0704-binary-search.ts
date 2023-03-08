dc290d28e393b88adfbf9ab36b20dcb04549c6c0
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-03-08 23:32:09 +0530

    Time: 62 ms (92.81%), Space: 46.2 MB (46.70%) - LeetHub

--- 0704-binary-search/0704-binary-search.ts
+++ 0704-binary-search/0704-binary-search.ts
@@ -1,12 +1,3 @@
 function search(nums: number[], target: number): number {
-  let low: number =0;
-    let high: number =nums.length-1;
-    while(low<=high){
-        const mid: number = Math.floor( (low+(high-low)/2));
-        console.log(mid);
-        if(nums[mid]==target) return mid;
-        else if(nums[mid]>target) high = mid-1;
-        else low = mid+1;
-    }
-    return -1;
+  return nums.indexOf(target);
 };
