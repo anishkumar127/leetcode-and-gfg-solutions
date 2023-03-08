9bf2a7fd727f53cf10ee3c4ae7a3f1d9b227ca9a
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-03-08 23:04:16 +0530

    Time: 80 ms (84.00%), Space: 50.6 MB (23.63%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let sum =0;
+    let max =  Number. NEGATIVE_INFINITY;
+    
+    for(let value of nums){
+        sum +=value;
+        max= Math.max(max, sum);
+        if(sum<0) sum = 0;
+    }
+    return max;
+};
