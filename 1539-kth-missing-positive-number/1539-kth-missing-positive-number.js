c447cf93f54f9e821ba868d52ca24b60de0976f9
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-03-09 21:10:05 +0530

    Time: 57 ms (82.55%), Space: 42.2 MB (62.74%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
@@ -4,9 +4,12 @@
  * @return {number}
  */
 var findKthPositive = function(arr, k) {
-    for(const e of arr){
-        if(k>=e) k++;
-        else return k;
+    let s =0 , e  = arr.length-1, mid = 0;
+    
+    while(s<=e){
+        mid = Math.floor(s+(e-s)/2);
+        if(arr[mid]-1-mid<k) s = mid + 1;
+        else e = mid-1;
     }
-    return k;
+    return s+k;
 };
