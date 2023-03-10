6f0b129ef6e24e3dc6e26b4ad9abb7bb25f303b0
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-03-10 22:32:18 +0530

    Time: 13 ms (35.62%), Space: 44.6 MB (35.47%) - LeetHub

---
+++ 0382-linked-list-random-node/0382-linked-list-random-node.java
@@ -0,0 +1,24 @@
+
+public class Solution {
+    
+    ListNode head;
+    Random random;
+    
+    public Solution(ListNode h) {
+        head = h;       
+        random = new Random();        
+    }
+    
+    public int getRandom() {
+        
+        ListNode c = head;
+        int r = c.val;
+        for(int i=1;c.next != null;i++){
+            
+            c = c.next;
+            if(random.nextInt(i + 1) == i) r = c.val;                        
+        }
+        
+        return r;
+    }
+}
