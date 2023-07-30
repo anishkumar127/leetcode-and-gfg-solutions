489d0f7e7117714457d31602285565093196e648
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-07-30 22:54:58 +0530

    Time: 70 ms (70.71%), Space: 49.5 MB (13.77%) - LeetHub

--- 0136-single-number/0136-single-number.ts
+++ 0136-single-number/0136-single-number.ts
@@ -1,3 +1,12 @@
 function singleNumber(nums: number[]): number {
-   return nums.reduce((prev,currValue)=> prev^currValue,0);
-}
+ const map = new Map();
+   nums.forEach((value)=>{
+       const freq = (map.get(value) ?? 0 ) + 1;
+       map.set(value,freq);
+   })
+    for(const [key,value] of map){
+        // console.log([key,value]); / [2,1]
+        if(value===1) return key;
+    }
+    // return -1;
+};
