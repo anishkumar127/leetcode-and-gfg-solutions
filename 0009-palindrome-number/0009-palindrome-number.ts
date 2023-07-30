6a68e708934e1ce80f54e042faeba603b7096852
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-07-30 22:11:56 +0530

    Time: 147 ms (97.83%), Space: 51.9 MB (54.32%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.ts
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -1,11 +1,12 @@
 function isPalindrome(x: number): boolean {
- const strConverted = x.toString();
- let s = 0;
- let e = strConverted.length-1;
-    while(s<=e){
-        if(strConverted[s++]!== strConverted[e--]){
-            return false;
-        }
+   if(x<0) return false;
+  const strConverted = x.toString();
+  let s = 0;
+  let e = strConverted.length - 1;
+  while (s <= e) {
+    if (strConverted[s++] !== strConverted[e--]) {
+      return false;
     }
-    return true;
-};
+  }
+  return true;
+}
