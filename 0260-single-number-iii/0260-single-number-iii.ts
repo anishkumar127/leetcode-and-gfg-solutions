3ba6e2d36ef7b65c7fc56ec2fa50acaf5737d423
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-07-30 23:34:57 +0530

    Time: 81 ms (33.33%), Space: 47.3 MB (10.00%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.ts
+++ 0260-single-number-iii/0260-single-number-iii.ts
@@ -1,13 +1,11 @@
 function singleNumber(nums: number[]): number[] {
-  const map = new Map();
+  const map = new Map<number, number>();
   for (const i in nums) {
     map.set(nums[i], (map.get(nums[i]) ?? 0) + 1);
   }
-  let arr = [];
-  for (const [key, value] of map) {
-    if (value === 1) {
-      arr.push(key);
-    }
+  const ans: number[] = [];
+  for (const [key, value] of map.entries()) {
+    if (value === 1) ans.push(key);
   }
-  return arr;
+  return ans;
 }
