a84cd2831fb18b367820a1cf2850a8a57d33dbaa
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-07-31 21:51:05 +0530

    Time: 112 ms (35.80%), Space: 67.1 MB (6.17%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.ts
@@ -0,0 +1,12 @@
+function findDuplicates(nums: number[]): number[] {
+  const arr = [];
+  const set = new Set();
+  for (const i in nums) {
+    if (set.has(nums[i])) {
+      arr.push(nums[i]);
+    } else {
+      set.add(nums[i]);
+    }
+  }
+  return arr;
+}
