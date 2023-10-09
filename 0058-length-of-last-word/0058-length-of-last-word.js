6c8f1452ec7136c042da28c8adbd92066b162015
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-10-09 22:18:31 +0530

    Time: 57 ms (19.80%), Space: 42.2 MB (26.86%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -2,13 +2,15 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function(s) {
-    s = s.trim();
-    let count =0;
-    for(let i=s.length-1; i>=0; i--){
-        if(s.charAt(i)!==' ')
-            count++;
-        else break;
-    }
-    return count;
+var lengthOfLastWord = function (s) {
+  const word = s.trim();
+  let ans = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    //console.log(word[i]);
+    if (word[i] !== " ") ans += word[i];
+    else break;
+  }
+
+  //(ans = ans.split("").reverse().join(""));
+  return ans.length;
 };
