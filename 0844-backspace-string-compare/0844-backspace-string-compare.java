ae046d204877efd92261ae9c05147943f19a02f7
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-10-20 00:27:37 +0530

    Time: 5 ms (9.90%), Space: 40.5 MB (54.97%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    public boolean backspaceCompare(String s, String t) {
+        StringBuilder sb = new StringBuilder(s);
+        StringBuilder sb2 = new StringBuilder(t);
+
+        for (int i = 0; i < sb.length(); i++) {
+             if (sb.charAt(0) == '#') {
+                sb.delete(0, 1);
+            }
+            else if (sb.charAt(i) == '#') {
+                if (i > 0 && i < sb.length()) {
+                    sb.delete(i - 1, i + 1);
+                    // if(sb.length()>2){
+                    i--;
+                    i--;
+                    // }
+                }
+            }
+        }
+        for (int i = 0; i < sb2.length(); i++) {
+            if (sb2.charAt(0) == '#') {
+                sb2.delete(0, 1);
+            } else if (sb2.charAt(i) == '#') {
+                if (i > 0 && i < sb2.length()) {
+                    sb2.delete(i - 1, i + 1);
+                    i--;
+                    i--;
+                }
+            }
+        }
+        System.out.print(sb.toString());
+        System.out.print("=>");
+
+        System.out.print(sb2.toString());
+
+        boolean x = sb.toString().equals(sb2.toString());
+        System.out.print(x);
+        // return  sb.toString().equals(sb2.toString());
+        return x;
+    }
+}
