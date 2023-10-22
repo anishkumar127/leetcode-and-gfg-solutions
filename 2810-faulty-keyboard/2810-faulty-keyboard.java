cab6ea09a20bdb5ae9b408749647bb1f2149af9c
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-10-22 23:44:23 +0530

    Time: 47 ms (5.34%), Space: 44.1 MB (25.70%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public String finalString(String s) {
+        StringBuilder sb = new StringBuilder(s);
+
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == 'i') {
+                String deleted = sb.substring(0, i);
+                sb.delete(0, i + 1);
+                StringBuilder del = new StringBuilder(deleted);
+                del.reverse();
+                sb.insert(0,del);
+                System.out.print(del);
+                i = 0;
+            }
+        }
+        return sb.toString();
+    }
+}
